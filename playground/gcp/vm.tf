d7f4ef2859ee46af25cc193f08bb7bef347bd467
Julian Schallenmueller <Julian.Schallenmueller@novatec-gmbh.de>
2021-11-22 14:55:23 +0100

    Removed old credentials string

--- playground/gcp/vm.tf
+++ playground/gcp/vm.tf
@@ -12,8 +12,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/home/julian/Documents/gcp/active-woodland-324808-271662d37497.json")
-
   project = "active-woodland-324808"
   region  = "europe-west3"
   zone    = "europe-west3-b"
