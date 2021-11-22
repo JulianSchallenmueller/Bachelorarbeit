1dc0ce4f6a9d2661a510286c88ef14781a4c30e4
Julian Schallenmueller <Julian.Schallenmueller@novatec-gmbh.de>
2021-11-22 12:30:26 +0100

    Some Test infrstructure for workflow testing

---
+++ playground/gcp/vm.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.1.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/julian/Documents/gcp/active-woodland-324808-271662d37497.json")
+
+  project = "active-woodland-324808"
+  region = "europe-west3"
+  zone = "europe-west3-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "vm-network"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_instance" "vm" {
+  name = "vm-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-minimal-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
